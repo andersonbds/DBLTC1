4a185789d004b36b32f31264d8c5d71610f94a57
andersonbds <1andersonsouaa@gmail.com>
2025-05-24 20:27:10 -0300

    Update and rename teste_import.py to afiliados.py

---
+++ afiliados.py
@@ -0,0 +1,2 @@
+def mostrar_indicacao(user_id):
+    return "Você ainda não indicou ninguém. Compartilhe seu link de indicação para ganhar bônus!"
--- teste_import.py
+++
@@ -1,4 +0,0 @@
-from database import Database
-
-db = Database()
-print("Import e instanciação da classe Database funcionou!")
